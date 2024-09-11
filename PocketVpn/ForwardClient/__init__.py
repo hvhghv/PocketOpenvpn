1368c69542cfd0effbd7d7019948867c8eb734b9
hvhghv <1064591880@qq.com>
2024-09-11 16:49:52 +0800

    "重构"

--- PocketVpn/ForwardClient/__init__.py
+++
@@ -1,5 +0,0 @@
-from .VpnForwardClientContext import ProjectContext
-
-
-class ForwardClient(ProjectContext):
-    pass
--- PocketVpn/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ForwardServer.simpleForwardServer import ForwardServer
-from .Vpn import VpnSocket
-from .ForwardClient import ForwardClient
